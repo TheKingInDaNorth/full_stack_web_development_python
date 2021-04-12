fa2ac6f91a6fd7b86a28362af92816f099d412b5
TheKingInDaNorth <60161055+TheKingInDaNorth@users.noreply.github.com>
2021-04-12 09:05:10 -0700

    4,12,21

--- web2py_win_py37/web2py/applications/pluralsight/controllers/contacts.py
+++ web2py_win_py37/web2py/applications/pluralsight/controllers/contacts.py
@@ -2,6 +2,15 @@
 # try something like
 def index(): return dict(message="hello from contacts.py")
 
-def data():
-    rows = db(db.contacts).select()
+def filter():
+    rows1_count = db(db.contacts.state_name=='CA').count()
+    
+    rows2_all_sorted_by_name = db(db.contacts).select(orderby=~db.contacts.last_name | db.contacts.first_name)
+    
+    row3_startswith = db(db.contacts.last_name.startswith('M')).select(orderby=db.contacts.state_name | db.contacts.last_name)
+    
+    row4_by_state = db(~(db.contacts.state_name=='CA')).select(orderby=db.contacts.last_name | db.contacts.first_name)
+    
+    row5_combo = db((db.contacts.state_name=='CA') & (db.contacts.last_name.startswith('A'))).select(orderby=db.contacts.last_name)
+
     return locals()
