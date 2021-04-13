17fe22ff7f0f2724414ee92afa1fda5c7423c50c
TheKingInDaNorth <60161055+TheKingInDaNorth@users.noreply.github.com>
2021-04-13 10:56:41 -0700

    4,13,21

--- web2py_win_py37/web2py/applications/pluralsight/controllers/contacts.py
+++ web2py_win_py37/web2py/applications/pluralsight/controllers/contacts.py
@@ -14,3 +14,26 @@ def filter():
     row5_combo = db((db.contacts.state_name=='CA') & (db.contacts.last_name.startswith('A'))).select(orderby=db.contacts.last_name)
 
     return locals()
+
+def add():
+    form = SQLFORM(db.contacts).process()
+    return locals()
+
+def view():
+    if request.args(0) is None:
+        rows = db(db.contacts).select(orderby=db.contacts.last_name | db.contacts.first_name)
+    else:
+        letter = request.args(0)
+        rows = db(db.contacts.last_name.startswith(letter)).select(orderby=db.contacts.last_name | db.contacts.first_name)
+        
+    return locals()
+
+
+def update():
+    record = db.contacts(request.args(0)) or redirect(URL('view'))
+    form = SQLFORM(db.contacts, record)
+    if form.process().accepted:
+        response.flash = T('Record Updated')
+    else:
+        response.flash = T('Please complete the form.')
+    return locals()
